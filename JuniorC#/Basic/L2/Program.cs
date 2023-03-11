dbf19c3fb69e3d20caa5a1eeab576dcf36308217
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-11 19:37:03 +0300

    L3 corrected remarks

--- JuniorC#/Basic/L2/Program.cs
+++ JuniorC#/Basic/L2/Program.cs
@@ -6,13 +6,13 @@ class Program
     // Console.SetCursorPosition(1, 20);
     Console.WriteLine("Приветствую!");
     Console.WriteLine("\nКак тебя зовут?: ");
-    var name = Console.ReadLine();
+    string name = Console.ReadLine();
     Console.WriteLine("\nСколько тебе лет?: ");
-    var age = Convert.ToInt32(Console.ReadLine());
+    int age = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("\nГде ты работаешь?: ");
-    var workPlace = Console.ReadLine();
+    string workPlace = Console.ReadLine();
     Console.WriteLine("\nКакой твой знак зодиака?: ");
-    var zodiac = Console.ReadLine();
+    string zodiacSign = Console.ReadLine();
     Console.WriteLine($"\n\nТебя зовут {name}, тебе {age} лет, твой знак - {zodiac} и ты работаешь в/на {workPlace}!");
 
     Console.ReadKey();
