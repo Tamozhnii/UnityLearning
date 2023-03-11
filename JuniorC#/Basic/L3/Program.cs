cc6b88f8772c5805537b4f7e83882fe096bdf532
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-11 19:40:30 +0300

    L3

---
+++ JuniorC#/Basic/L3/Program.cs
@@ -0,0 +1,6 @@
+﻿int album = 52;
+int imageZone = 3;
+int entireRows = album / imageZone;
+int remainingImages = album % imageZone;
+
+Console.WriteLine($"Всего будет {entireRows} полных рядов, и еще останется {remainingImages} изображение(-ий)");
