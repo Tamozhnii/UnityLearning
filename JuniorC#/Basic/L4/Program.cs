f36235f3938539bb7e3a8c61227ee077b753b8aa
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-11 20:07:17 +0300

    L4

---
+++ JuniorC#/Basic/L4/Program.cs
@@ -0,0 +1,23 @@
+﻿//С использованием 3-ей переменной
+string firstName = "Tarantino";
+string surname = "Quentin";
+
+Console.WriteLine($"Имя - {firstName} и фамилия - {surname}");
+
+string swapBuffer = firstName;
+firstName = surname;
+surname = swapBuffer;
+
+Console.WriteLine($"Имя - {firstName} и фамилия - {surname}\n");
+
+//Без использования 3-ей переменной
+firstName = "Ritchie";
+surname = "Guy";
+
+Console.WriteLine($"Имя - {firstName} и фамилия - {surname}");
+
+firstName += surname;
+surname = firstName.Replace(surname, "");
+firstName = firstName.Replace(surname, "");
+
+Console.WriteLine($"Имя - {firstName} и фамилия - {surname}");
