3d3905bf34f98d4ffff5b2a81e4a237aa897d0c6
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-11 11:57:15 +0300

    fix script

--- JuniorC#/Basic/L1/Variables.cs
+++ JuniorC#/Basic/L1/Variables.cs
@@ -1,35 +1,23 @@
-using System;
-
-namespace L1
+string nickname = "Terminator2000";
+int age = 14;
+uint coins = 10;
+float progress = 10.5f;
+decimal virtualMoney = 100.5m;
+bool isVerification = true;
+List<Stickers> stickers = new List<Stickers>() { Stickers.SubZero, Stickers.Kitana, Stickers.Goro };
+var memoryBuffer;
+Random rand = new Random();
+char[,] map = new char[3, 5] {
+  { '*', '*', ' ', ' ', ' ' },
+  { ' ', '*', '*', '*', ' ' },
+  { ' ', ' ', ' ', '*', '*' }
+};
+enum Stickers
 {
-  public static class Program
-  {
-    public static void Main(string[] args)
-    {
-      string nickname = "Terminator2000";
-      int age = 14;
-      uint coins = 10;
-      float progress = 10.5f;
-      decimal virtualMoney = 100.5m;
-      bool isVerification = true;
-      List<Stickers> stickers = new List<Stickers>() { Stickers.SubZero, Stickers.Kitana, Stickers.Goro };
-      var memoryBuffer;
-      Random rand = new Random();
-      char[,] map = new char[3, 5] {
-        { '*', '*', ' ', ' ', ' ' },
-        { ' ', '*', '*', '*', ' ' },
-        { ' ', ' ', ' ', '*', '*' }
-      };
-    }
-
-    enum Stickers
-    {
-      SubZero,
-      Melina,
-      Kitana,
-      Goro,
-      ShaoKahn,
-      Raiden
-    }
-  }
+  SubZero,
+  Melina,
+  Kitana,
+  Goro,
+  ShaoKahn,
+  Raiden
 }
