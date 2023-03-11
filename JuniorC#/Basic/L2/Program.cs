fbcc0f1bb3c3e7a8740256c1a495e83c5cbec7bc
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-11 16:10:10 +0300

    add lesson 2

---
+++ JuniorC#/Basic/L2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace L2;
+class Program
+{
+  static void Main(string[] args)
+  {
+    // Console.SetCursorPosition(1, 20);
+    Console.WriteLine("Приветствую!");
+    Console.WriteLine("\nКак тебя зовут?: ");
+    var name = Console.ReadLine();
+    Console.WriteLine("\nСколько тебе лет?: ");
+    var age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\nГде ты работаешь?: ");
+    var workPlace = Console.ReadLine();
+    Console.WriteLine("\nКакой твой знак зодиака?: ");
+    var zodiac = Console.ReadLine();
+    Console.WriteLine($"\n\nТебя зовут {name}, тебе {age} лет, твой знак - {zodiac} и ты работаешь в/на {workPlace}!");
+
+    Console.ReadKey();
+  }
+}
