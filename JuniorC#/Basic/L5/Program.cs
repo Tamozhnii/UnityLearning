9e8db90dbb511a7de3df5e8a549690272a7bf6db
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-12 01:49:36 +0300

    L5

---
+++ JuniorC#/Basic/L5/Program.cs
@@ -0,0 +1,17 @@
+﻿int crystalCount = 0;
+int pricePerCrystal = 7;
+
+Console.Write("Введите сколько у вас в наличии золота: ");
+int goldAmount = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+Console.WriteLine("\nДобро пожаловать в лавку!\n");
+Console.WriteLine($"Здраствуйте, стоимость 1 кристалла составляет {pricePerCrystal} золотых.");
+Console.WriteLine("Сколько кристаллов вы желаете приобрести?");
+int desiredAmount = Convert.ToInt32(Console.ReadLine());
+
+goldAmount -= desiredAmount * pricePerCrystal;
+crystalCount = desiredAmount;
+
+Console.WriteLine($"\nПоздравляем за покупку, вы получили {crystalCount} кристаллов и у вас осталось {goldAmount} золота.");
+Console.ReadKey();
