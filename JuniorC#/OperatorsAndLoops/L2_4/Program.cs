e838b38a9d4502fa2a9744766cb8e2c142b6829a
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-12 21:33:17 +0300

    L2_4 corrected remarks

--- JuniorC#/OperatorsAndLoops/L2_4/Program.cs
+++ JuniorC#/OperatorsAndLoops/L2_4/Program.cs
@@ -1,17 +1,19 @@
-﻿int sum = 0;
+﻿int minValue = 0;
+int maxValue = 100;
 Random rand = new Random();
-int randomValue = rand.Next(0, 100);
+int randomValue = rand.Next(minValue, maxValue);
+int basis1 = 3;
+int basis2 = 5;
+int sum = 0;
 
 for (int i = 0; i <= randomValue; i++)
 {
-  bool isValidNumber = i % 3 == 0 || i % 5 == 0;
-
-  if (isValidNumber)
+  if (i % basis1 == 0 || i % basis2 == 0)
   {
     sum += i;
   }
 }
 
 Console.WriteLine($"Случайное значение равно {randomValue}");
-Console.WriteLine($"Сумма всех положительных чисел кратных 3 или 5 состовляет {sum}");
+Console.WriteLine($"Сумма всех положительных чисел кратных {basis1} или {basis2} состовляет {sum}");
 Console.ReadKey();
