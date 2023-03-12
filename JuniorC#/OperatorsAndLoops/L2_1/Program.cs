6b3672f9a343000f3bbd196d7d1ff8cf970f7444
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-12 14:37:11 +0300

    L2_1 corrected remarks

--- JuniorC#/OperatorsAndLoops/L2_1/Program.cs
+++ JuniorC#/OperatorsAndLoops/L2_1/Program.cs
@@ -3,8 +3,10 @@ string userMessage = Console.ReadLine();
 Console.Write("Сколько раз вывести данное сообщение?: ");
 int cyclesCount = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
+
 for (int i = 1; i <= cyclesCount; i++)
 {
   Console.WriteLine($"{i}. {userMessage}");
 }
+
 Console.ReadKey();
