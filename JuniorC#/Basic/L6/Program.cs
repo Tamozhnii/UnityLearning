f1123e6e3172dd66e534313b18e77bd4315eb1b7
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-12 13:46:14 +0300

    L6 corrected remarks

--- JuniorC#/Basic/L6/Program.cs
+++ JuniorC#/Basic/L6/Program.cs
@@ -3,7 +3,7 @@ int receptionTimeInMinutes = 10;
 int hospitalQueue = 14;
 
 int waitingTime = hospitalQueue * receptionTimeInMinutes;
-int hours = waitingTime / minutesPerHour;
-int minutes = waitingTime % minutesPerHour;
+int waitingHours = waitingTime / minutesPerHour;
+int waitingMinutes = waitingTime % minutesPerHour;
 
-Console.WriteLine($"Вы должны отстоять в очереди {hours} часа и {minutes} минут.");
+Console.WriteLine($"Вы должны отстоять в очереди {waitingHours} часа и {waitingMinutes} минут.");
