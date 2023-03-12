5e1c59150f3f47d4fb183c629da0dce3ad8527f7
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-12 13:44:39 +0300

    L2_1

---
+++ JuniorC#/OperatorsAndLoops/L2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите желаемое сообщение: ");
+string userMessage = Console.ReadLine();
+Console.Write("Сколько раз вывести данное сообщение?: ");
+int cyclesCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+for (int i = 1; i <= cyclesCount; i++)
+{
+  Console.WriteLine($"{i}. {userMessage}");
+}
+Console.ReadKey();
