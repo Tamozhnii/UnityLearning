aed7cc9f30dcd06a60326f764af6a8d3aa1bbbe7
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-12 16:10:19 +0300

    L2_4

---
+++ JuniorC#/OperatorsAndLoops/L2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿int sum = 0;
+Random rand = new Random();
+int randomValue = rand.Next(0, 100);
+
+for (int i = 0; i <= randomValue; i++)
+{
+  bool isValidNumber = i % 3 == 0 || i % 5 == 0;
+
+  if (isValidNumber)
+  {
+    sum += i;
+  }
+}
+
+Console.WriteLine($"Случайное значение равно {randomValue}");
+Console.WriteLine($"Сумма всех положительных чисел кратных 3 или 5 состовляет {sum}");
+Console.ReadKey();
