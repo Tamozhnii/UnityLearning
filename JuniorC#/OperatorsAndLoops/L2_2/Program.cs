11157d3643cab41e377cc65ec26203642049066a
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-12 14:34:36 +0300

    L2_2

---
+++ JuniorC#/OperatorsAndLoops/L2_2/Program.cs
@@ -0,0 +1,9 @@
+﻿string userCommand = "";
+
+Console.WriteLine("Добро пожаловать в VIM");
+
+while (userCommand.ToLower() != "exit")
+{
+  Console.Write("Введите команду: ");
+  userCommand = Console.ReadLine();
+}
