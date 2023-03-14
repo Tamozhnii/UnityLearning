9040e521b0f9cfe44bbc5fec76eff6fb806a38e9
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-14 21:00:13 +0300

    L2_7

---
+++ JuniorC#/OperatorsAndLoops/L2_7/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите имя: ");
+string userName = Console.ReadLine();
+Console.Write("Укажите желаемый символ: ");
+char symbol = Convert.ToChar(Console.ReadLine());
+int pictureFrameLenght = userName.Length + 2;
+string pictureFrame = "";
+
+for (int i = 0; i < pictureFrameLenght; i++)
+{
+  pictureFrame += symbol;
+}
+
+Console.WriteLine($"\n{pictureFrame}");
+Console.WriteLine($"{symbol}{userName}{symbol}");
+Console.WriteLine($"{pictureFrame}");
