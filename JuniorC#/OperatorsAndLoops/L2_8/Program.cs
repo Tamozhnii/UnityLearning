cab6c0440544b4e8ed62e9d725998ae3c0ed203a
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-14 21:39:48 +0300

    L2_8

---
+++ JuniorC#/OperatorsAndLoops/L2_8/Program.cs
@@ -0,0 +1,30 @@
+﻿string sectretPassword = "password";
+int attempts = 3;
+
+for (int i = 0; i < attempts; i++)
+{
+  Console.Write("Введите пароль: ");
+  string userPassword = Console.ReadLine();
+
+  if (userPassword.ToLower() == sectretPassword)
+  {
+    Console.WriteLine("\nВас приветствует тайное общество!");
+    Console.ReadKey();
+    break;
+  }
+  else
+  {
+    if (i < 2)
+    {
+      Console.WriteLine("\nНеверный пароль");
+    }
+    else
+    {
+      Console.WriteLine("\nВам здесь не рады!");
+    }
+
+    Console.ReadKey();
+  }
+
+  Console.Clear();
+}
