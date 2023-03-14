d581e3cadf928263312e99b215397df74ed3d1f1
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-14 22:31:04 +0300

    L2_9

---
+++ JuniorC#/OperatorsAndLoops/L2_9/Program.cs
@@ -0,0 +1,29 @@
+﻿int minValue = 1;
+int maxValue = 27;
+int numericalValue = 3;
+Random random = new Random();
+int currentNumber = random.Next(minValue, maxValue + 1);
+int otherNumber = 0;
+int resultCounter = 0;
+
+while (otherNumber.ToString().Length <= numericalValue)
+{
+  otherNumber += currentNumber;
+
+  if (otherNumber.ToString().Length == numericalValue)
+  {
+    resultCounter++;
+  }
+}
+
+Console.WriteLine($"Количество трехзначных натуральных чисел кратных {currentNumber} состовляет: {resultCounter}");
+
+//Проверка
+// int testCounter = 0;
+
+// for (int i = 100; i < 1000; i++)
+// {
+//   if (i % currentNumber == 0) testCounter++;
+// }
+
+// Console.WriteLine($"Тест: {testCounter}");
