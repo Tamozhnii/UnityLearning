1f393f5487df36d007915a9531a7f535d1ad89e3
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-16 00:38:01 +0300

    L2_11

---
+++ JuniorC#/OperatorsAndLoops/l2_11/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите строку для проверки: ");
+string userValue = Console.ReadLine();
+int deep = 0;
+int counter = 0;
+
+foreach (var symbol in userValue)
+{
+  if (symbol == '(')
+  {
+    counter++;
+  }
+  else if (symbol == ')')
+  {
+    counter--;
+  }
+
+  if (deep < counter)
+  {
+    deep = counter;
+  }
+
+  if (counter < 0)
+  {
+    break;
+  }
+}
+
+if (counter == 0)
+{
+  Console.WriteLine($"Строка корректная, глубина равняется {deep}");
+}
+else
+{
+  Console.WriteLine($"Строка не корректная");
+}
