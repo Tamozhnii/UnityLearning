b9ae6c4ec26dec774ae1ec1412d263ca5d39919c
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-15 23:52:10 +0300

    fix

--- JuniorC#/OperatorsAndLoops/L2_8/Program.cs
+++ JuniorC#/OperatorsAndLoops/L2_8/Program.cs
@@ -1,30 +1,28 @@
-﻿string sectretPassword = "password";
+﻿string secretPassword = "password";
 int attempts = 3;
 
-for (int i = 0; i < attempts; i++)
+for (int i = 1; i <= attempts; i++)
 {
   Console.Write("Введите пароль: ");
   string userPassword = Console.ReadLine();
 
-  if (userPassword.ToLower() == sectretPassword)
+  if (userPassword == secretPassword)
   {
     Console.WriteLine("\nВас приветствует тайное общество!");
     Console.ReadKey();
     break;
   }
+
+
+  if (i < attempts)
+  {
+    Console.WriteLine("\nНеверный пароль");
+  }
   else
   {
-    if (i < 2)
-    {
-      Console.WriteLine("\nНеверный пароль");
-    }
-    else
-    {
-      Console.WriteLine("\nВам здесь не рады!");
-    }
-
-    Console.ReadKey();
+    Console.WriteLine("\nВам здесь не рады!");
   }
 
+  Console.ReadKey();
   Console.Clear();
 }
