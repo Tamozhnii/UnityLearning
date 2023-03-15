a15351ce5da68a2ad5e2d110db4d722c9530668c
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-16 00:21:26 +0300

    L2_10

---
+++ JuniorC#/OperatorsAndLoops/L2_10/Program.cs
@@ -0,0 +1,13 @@
+﻿int currentNumber = 2;
+Random random = new Random();
+int randomNumber = random.Next(100);
+int extent = 0;
+double result = 0;
+
+for (int i = 0; result <= randomNumber; i++)
+{
+  result = Math.Pow(currentNumber, i);
+  extent = i;
+}
+
+Console.WriteLine($"number = {randomNumber}, extent = {extent}, result = {result}");
