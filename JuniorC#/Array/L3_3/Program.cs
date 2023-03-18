94cced44823aa26d0238c1582ac62e2e91fddc71
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-18 15:45:27 +0300

    L3_3

---
+++ JuniorC#/Array/L3_3/Program.cs
@@ -0,0 +1,30 @@
+﻿Random random = new Random();
+int maxValue = 10;
+int[] numbers = new int[30];
+
+Console.Write("[ ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+  numbers[i] = random.Next(maxValue);
+  Console.Write($"{numbers[i]} ");
+}
+
+Console.Write("]\n");
+Console.Write("Локальные максимальные числа: ");
+
+for (int j = 0; j < numbers.Length; j++)
+{
+  if (j == 0)
+  {
+    if (numbers[j] > numbers[j + 1]) Console.Write($"{numbers[j]} ");
+  }
+  else if (j == numbers.Length - 1)
+  {
+    if (numbers[j] > numbers[j - 1]) Console.Write($"{numbers[j]} ");
+  }
+  else
+  {
+    if (numbers[j] > numbers[j + 1] && numbers[j] > numbers[j - 1]) Console.Write($"{numbers[j]} ");
+  }
+}
