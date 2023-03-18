399d8a5560c63b2c5e8a6e8a34f6c319ab916190
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-18 16:53:50 +0300

    L3_5

---
+++ JuniorC#/Array/L3_5/Program.cs
@@ -0,0 +1,45 @@
+﻿Random random = new Random();
+int[] array = new int[30];
+int minValue = 1;
+int maxValue = 4;
+int value = 0;
+int maxCounter = 0;
+int currentValue = 0;
+int currentCounter = 1;
+
+Console.Write("[ ");
+
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = random.Next(minValue, maxValue);
+  Console.Write($"{array[i]} ");
+}
+
+Console.Write("]");
+
+foreach (int number in array)
+{
+  if (value == 0)
+  {
+    value = number;
+  }
+
+  if (number == currentValue)
+  {
+    currentCounter++;
+  }
+  else if (maxCounter < currentCounter)
+  {
+    value = currentValue;
+    maxCounter = currentCounter;
+    currentCounter = 1;
+  }
+  else
+  {
+    currentCounter = 1;
+  }
+
+  currentValue = number;
+}
+
+Console.WriteLine($"\nСамое большое число повторений цифры {value}, {maxCounter} раз(-а) подряд");
