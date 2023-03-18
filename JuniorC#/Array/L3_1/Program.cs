e23af5376ea6bcd3cc8a32a3b8df1770d0c6cea7
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-18 14:19:06 +0300

    L3_1

---
+++ JuniorC#/Array/L3_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[,] array = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+int sumOfSecondLine = 0;
+int secondLineIndex = 1;
+int multiplicationOfFirstColumn = 0;
+int firstColumnIndex = 0;
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+  sumOfSecondLine += array[secondLineIndex, i];
+}
+
+for (int j = 0; j < array.GetLength(0); j++)
+{
+  multiplicationOfFirstColumn += array[j, firstColumnIndex];
+}
+
+Console.WriteLine($"Сумма второй строки = {sumOfSecondLine}, произведение первого столбца = {multiplicationOfFirstColumn}");
