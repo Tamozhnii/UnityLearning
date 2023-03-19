4a66de4805f297d61675ecf44474ef15b2e35707
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-19 16:27:51 +0300

    L3_8 fix

--- JuniorC#/Array/L3_8/Program.cs
+++ JuniorC#/Array/L3_8/Program.cs
@@ -1,6 +1,7 @@
 ﻿int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
 Console.Write("Укажите на сколько сдвинуть массив влево: ");
 int offsetToLeft = Convert.ToInt32(Console.ReadLine());
+int firstElementIndex = 0;
 
 Console.Write("[ ");
 
@@ -13,12 +14,14 @@ Console.Write("]");
 
 for (int f = 0; f < offsetToLeft; f++)
 {
-  int temp = numbers[0];
+  int firstElementValue = numbers[firstElementIndex];
+
   for (int i = 0; i < numbers.Length - 1; i++)
   {
     numbers[i] = numbers[i + 1];
   }
-  numbers[numbers.Length - 1] = temp;
+
+  numbers[numbers.Length - 1] = firstElementValue;
 }
 
 Console.Write(" => [ ");
