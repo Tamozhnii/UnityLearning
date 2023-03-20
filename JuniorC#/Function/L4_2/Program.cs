a3b2ee8050165d81084e3129a0e52a4970e621a8
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-21 00:27:28 +0300

    L4_2

---
+++ JuniorC#/Function/L4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿int maxHealthPoints = 20;
+int currentHealthPointsInPercent = 75;
+
+void ShowHealthbar(int healthPointsInPercent)
+{
+  int currentHealthPoints = Convert.ToInt32(maxHealthPoints * healthPointsInPercent / 100f);
+  Console.Write("[");
+
+  for (int i = 0; i < currentHealthPoints; i++)
+  {
+    Console.Write("†");
+  }
+
+  for (int i = currentHealthPoints; i < maxHealthPoints; i++)
+  {
+    Console.Write("_");
+  }
+
+  Console.Write("]");
+}
+
+ShowHealthbar(currentHealthPointsInPercent);
