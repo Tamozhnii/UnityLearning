c530cc6dd665ecd7daa46ceea023d58b79f1647f
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-21 21:25:45 +0300

    L4_3

---
+++ JuniorC#/Function/L4_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace L4_3
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Program program = new Program();
+      Console.WriteLine(program.ConvertToInt());
+    }
+
+    private int ConvertToInt()
+    {
+      string userInput = "";
+      int number = 0;
+
+      while (userInput != number.ToString())
+      {
+        Console.Write("Введите число: ");
+        userInput = Console.ReadLine();
+        int.TryParse(userInput, out number);
+      }
+
+      return number;
+    }
+  }
+
+}
