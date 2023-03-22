12d2e3addbb0ae3fc18d6a6edb71d2511873f519
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-23 00:22:55 +0300

    L4_2 corrected remarks

--- JuniorC#/Function/L4_2/Program.cs
+++ JuniorC#/Function/L4_2/Program.cs
@@ -1,22 +1,34 @@
-﻿int maxHealthPoints = 20;
-float currentHealthPointsInPercent = 0.75f;
-int startPosition = 0;
+﻿using System;
 
-void DrowBar(int startPosition, int lastPosition, char symbol)
+namespace L4_2
 {
-  for (int i = startPosition; i < lastPosition; i++)
+  class Program
   {
-    Console.Write(symbol);
+    private static void Main(string[] args)
+    {
+
+      int healthbarLength = 20;
+      int currentHealthPointsInPercent = 66;
+      ShowHealthbar(currentHealthPointsInPercent, healthbarLength);
+    }
+
+    private static void DrawBar(int startPosition, int lastPosition, char symbol)
+    {
+      for (int i = startPosition; i < lastPosition; i++)
+      {
+        Console.Write(symbol);
+      }
+    }
+
+    private static void ShowHealthbar(int healthPointsInPercent, int healthbarLength)
+    {
+      float maxHealthPointsInPercent = 100f;
+      int startPosition = 0;
+      int currentHealthPoints = Convert.ToInt32(healthbarLength / maxHealthPointsInPercent * healthPointsInPercent);
+      Console.Write("[");
+      DrawBar(startPosition, currentHealthPoints, '†');
+      DrawBar(currentHealthPoints, healthbarLength, '_');
+      Console.Write("]");
+    }
   }
 }
-
-void ShowHealthbar(float healthPointsInPercent)
-{
-  int currentHealthPoints = Convert.ToInt32(maxHealthPoints * healthPointsInPercent);
-  Console.Write("[");
-  DrowBar(startPosition, currentHealthPoints, '†');
-  DrowBar(currentHealthPoints, maxHealthPoints, '_');
-  Console.Write("]");
-}
-
-ShowHealthbar(currentHealthPointsInPercent);
