ee855417e6a1860f9196761dde609357b8669ea9
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-25 10:39:48 +0300

    L5_1

---
+++ JuniorC#/Collection/L5_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace L5_1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Dictionary<string, string> fruits = new Dictionary<string, string>();
+      fruits.Add("Арбуз", "это ягода");
+      fruits.Add("Помидор", "это ягода");
+      fruits.Add("Банан", "это ягода");
+      TakeAnswer(fruits);
+    }
+
+    private static void TakeAnswer(Dictionary<string, string> dictionary)
+    {
+      const string CommandExit = "Выйти";
+      const string CommandWatermelon = "Арбуз";
+      const string CommandTomato = "Помидор";
+      const string CommandBanana = "Банан";
+
+      bool isAsking = true;
+
+      while (isAsking)
+      {
+        Console.WriteLine("Введите название плода:");
+        string userInput = Console.ReadLine();
+
+        switch (userInput)
+        {
+          case CommandWatermelon:
+          case CommandTomato:
+          case CommandBanana:
+            Console.WriteLine(dictionary[userInput]);
+            break;
+
+          case CommandExit:
+            isAsking = false;
+            break;
+
+          default:
+            Console.WriteLine("Такой плод не найден, попробуйте снова");
+            break;
+        }
+      }
+    }
+  }
+}
