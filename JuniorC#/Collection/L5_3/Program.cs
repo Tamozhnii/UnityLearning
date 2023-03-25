19afa7af7645d9f86acba53e38bd541422fcfbc8
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-26 01:18:55 +0300

    L5_3

---
+++ JuniorC#/Collection/L5_3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace L5_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      const string CommandSum = "Sum";
+      const string CommandExit = "Exit";
+
+      bool isWork = true;
+      List<int> numbers = new List<int>();
+      Console.WriteLine("Введите число или команду:");
+
+      while (isWork)
+      {
+        string userInput = Console.ReadLine();
+
+        switch (userInput)
+        {
+          case CommandSum:
+            GetSum(numbers);
+            break;
+
+          case CommandExit:
+            isWork = false;
+            break;
+
+          default:
+            AddNumber(numbers, userInput);
+            break;
+        }
+      }
+    }
+
+    static void GetSum(List<int> numbers)
+    {
+      int result = 0;
+
+      foreach (int number in numbers)
+      {
+        result += number;
+      }
+
+      Console.WriteLine($"Сумма: {result}\n");
+    }
+
+    static void AddNumber(List<int> numbers, string input)
+    {
+      int number = 0;
+
+      if (int.TryParse(input, out number))
+      {
+        numbers.Add(number);
+      }
+      else
+      {
+        Console.WriteLine("Введите целое число");
+      }
+    }
+  }
+}
