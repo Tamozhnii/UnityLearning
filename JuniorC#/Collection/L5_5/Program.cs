3714fee4629cce17846445e813a44005d421e2f8
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-03-26 19:31:10 +0300

    L5_5

---
+++ JuniorC#/Collection/L5_5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace L5_5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] array1 = { "1", "2", "1" };
+      string[] array2 = { "3", "2", "3", "4" };
+      ConcatToUniqArray(array1, array2);
+    }
+
+    private static void ConcatToUniqArray(string[] array1, string[] array2)
+    {
+      Dictionary<string, string> uniqArray = new Dictionary<string, string>();
+      AddArray(array1, uniqArray);
+      AddArray(array2, uniqArray);
+      ShowArray(uniqArray);
+    }
+
+    private static void AddArray(string[] array, Dictionary<string, string> dictionary)
+    {
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (!dictionary.ContainsKey(array[i]))
+        {
+          dictionary.Add(array[i], array[i]);
+        }
+      }
+    }
+
+    private static void ShowArray(Dictionary<string, string> dictionary)
+    {
+      Console.Write("[ ");
+
+      foreach (var el in dictionary)
+      {
+        Console.Write($"{el.Value} ");
+      }
+
+      Console.Write("]");
+    }
+  }
+}
