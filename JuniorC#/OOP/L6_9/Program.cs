ff4cb4e53cd4cc996607c581c6f0b941731e5a08
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-05-20 01:02:11 +0300

    L6_9 fixed remarks

--- JuniorC#/OOP/L6_9/Program.cs
+++ JuniorC#/OOP/L6_9/Program.cs
@@ -13,28 +13,112 @@ namespace L6_9
 
     class Product
     {
-        private string _name;
+        private Groceries _name;
         private int _price;
 
-        public Product(string name, int price)
+        public Product(Groceries name, int price)
         {
             _name = name;
             _price = price;
         }
 
-        public string Name => _name;
+        public Groceries Name => _name;
         public int Price => _price;
     }
 
     class Customer
     {
+        private Random _random;
         private List<Product> _products;
         private int _wallet;
 
-        public Customer(int money)
+        public Customer(int money, List<Product> assortment)
         {
+            _random = new Random();
             _products = new List<Product>();
             _wallet = money;
+            PickUpProducts(assortment);
+        }
+
+        public void ShowProducts()
+        {
+            Console.Write("В корзине у клиента: ");
+
+            if (_products.Count > 0)
+            {
+                foreach (Product item in _products)
+                {
+                    Console.Write($"{item.Name} ");
+                }
+            }
+            else
+            {
+                Console.Write("Пусто");
+            }
+
+            Console.WriteLine();
+        }
+
+        public int Pay()
+        {
+            int empty = 0;
+            int totalCost = CalculateTotalCost();
+            int cash = empty;
+
+            if (totalCost == empty)
+            {
+                Console.WriteLine("Клиент ничего не приобрел");
+            }
+            else if (_wallet == empty)
+            {
+                Console.WriteLine("У клиента нет средств для оплаты товаров");
+            }
+            else
+            {
+                for (int i = _products.Count; i > 0; i--)
+                {
+                    if (totalCost > _wallet)
+                    {
+                        Product? product = ThrowAwayRandomProduct();
+
+                        if (product != null)
+                        {
+                            totalCost -= product.Price;
+                            Console.WriteLine($"Клиент вытащил из корзины {product.Name}");
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (_products.Count == empty)
+                {
+                    Console.WriteLine("У клиента не хватило средств");
+                }
+                else if (totalCost <= _wallet)
+                {
+                    _wallet -= totalCost;
+                    Console.WriteLine($"Клиент оплатил товаров на сумму {totalCost}$");
+                    cash = totalCost;
+                }
+                else
+                {
+                    Console.WriteLine("У клиента не хватило средств на оплату");
+                }
+            }
+
+            return cash;
+        }
+
+        private void PickUpProducts(List<Product> assortment)
+        {
+            for (int i = 0; i < _random.Next(assortment.Count); i++)
+            {
+                int productId = _random.Next(assortment.Count);
+                _products.Add(new Product(assortment[productId].Name, assortment[productId].Price));
+            }
         }
 
         private int CalculateTotalCost()
@@ -51,128 +135,24 @@ namespace L6_9
 
         private Product? ThrowAwayRandomProduct()
         {
-            Random random = new Random();
-            Product? randomProduct = _products[random.Next(_products.Count)];
+            Product? randomProduct = _products[_random.Next(_products.Count)];
             _products.Remove(randomProduct);
             return randomProduct;
-        }
-
-        public int Pay()
-        {
-            int empty = 0;
-            int cash = empty;
-            int totalCost = CalculateTotalCost();
-
-            if (_wallet == empty)
-            {
-                Console.WriteLine("У клиента нет средств для оплаты товаров");
-            }
-            else if (totalCost <= _wallet)
-            {
-                _wallet -= totalCost;
-                Console.WriteLine($"Клиент оплатил товаров на сумму {totalCost}$");
-                cash = totalCost;
-            }
-            else
-            {
-                Product? product = ThrowAwayRandomProduct();
-
-                if (product != null)
-                {
-                    _products.Remove(product);
-                    Console.WriteLine($"Клиент вытащил из корзины {product.Name}");
-                    cash = Pay();
-                }
-                else
-                {
-                    Console.WriteLine("В корзине уже нет продуктов");
-                }
-            }
-
-            return cash;
-        }
-
-        public void PutProduct(Product product)
-        {
-            _products.Add(product);
-            Console.WriteLine($"{product.Name} добавлен в корзину");
         }
     }
 
     class Shop
     {
         private int _cash;
-        private List<Product> _groceries;
+        private List<Product> _assortment;
         private List<Customer> _customers;
 
         public Shop()
         {
             _cash = 0;
-            _groceries = new List<Product>();
+            _assortment = new List<Product>();
             _customers = new List<Customer>();
             Initialize();
-        }
-
-        private void Initialize()
-        {
-            _groceries.Add(new Product("Хлеб", 1));
-            _groceries.Add(new Product("Молоко", 3));
-            _groceries.Add(new Product("Яйцо", 4));
-            _groceries.Add(new Product("Бананы", 2));
-            _groceries.Add(new Product("Мясо", 5));
-            Random random = new Random();
-            int maxCustomers = 5;
-            int minCash = 0;
-            int maxCash = 16;
-
-            for (int i = 0; i < maxCustomers; i++)
-            {
-                _customers.Add(new Customer(random.Next(minCash, maxCash)));
-            }
-        }
-
-        private void ToCash(int cash)
-        {
-            _cash += cash;
-            Console.WriteLine($"Операция завершена, в кассе {_cash}$\n");
-        }
-
-        private void SellProducts(Customer customer)
-        {
-            const string CommandPay = "pay";
-
-            bool isChoice = true;
-            Console.WriteLine("перечень продуктов:");
-
-            for (int i = 0; i < _groceries.Count; i++)
-            {
-                Product product = _groceries[i];
-                Console.WriteLine($"{i}. {product.Name} - {product.Price}$");
-            }
-
-            while (isChoice)
-            {
-                int id = -1;
-                Console.Write($"Выберите товар или введите {CommandPay} для оплаты:");
-                string inputCustomer = Console.ReadLine();
-                bool validCommand = int.TryParse(inputCustomer, out id);
-
-                if (validCommand && Math.Abs(id) < _groceries.Count)
-                {
-                    customer.PutProduct(_groceries[Math.Abs(id)]);
-                }
-                else if (inputCustomer == CommandPay)
-                {
-                    isChoice = false;
-                }
-                else
-                {
-                    Console.WriteLine("Такого товара нет, выберите снова");
-                }
-            }
-
-            int cash = customer.Pay();
-            ToCash(cash);
         }
 
         public void Begin()
@@ -181,10 +161,45 @@ namespace L6_9
             {
                 int clientNumber = i + 1;
                 Console.WriteLine($"Клиент - {clientNumber}");
-                SellProducts(_customers[i]);
+                _customers[i].ShowProducts();
+                int cash = _customers[i].Pay();
+                MakePayment(cash);
             }
 
             Console.WriteLine("Все клиенты обслужены!");
         }
+
+        private void Initialize()
+        {
+            _assortment.Add(new Product(Groceries.Bread, 1));
+            _assortment.Add(new Product(Groceries.Milk, 3));
+            _assortment.Add(new Product(Groceries.Eggs, 4));
+            _assortment.Add(new Product(Groceries.Apples, 2));
+            _assortment.Add(new Product(Groceries.Meat, 5));
+            Random random = new Random();
+            int maxCustomers = 5;
+            int minCash = 0;
+            int maxCash = 16;
+
+            for (int i = 0; i < maxCustomers; i++)
+            {
+                _customers.Add(new Customer(random.Next(minCash, maxCash), _assortment));
+            }
+        }
+
+        private void MakePayment(int cash)
+        {
+            _cash += cash;
+            Console.WriteLine($"Операция завершена, в кассе {_cash}$\n");
+        }
+    }
+
+    enum Groceries
+    {
+        Bread,
+        Milk,
+        Meat,
+        Apples,
+        Eggs
     }
 }
