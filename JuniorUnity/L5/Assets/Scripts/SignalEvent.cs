1f02fc65209e1a9ccf829bcd25f8d7f331639ee6
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2023-09-03 22:31:06 +0300

    L5 added Coroutine

--- JuniorUnity/L5/Assets/Scripts/SignalEvent.cs
+++ JuniorUnity/L5/Assets/Scripts/SignalEvent.cs
@@ -1,29 +1,24 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class SignalEvent : MonoBehaviour
 {
     [SerializeField] private UnityEvent _reached;
     [SerializeField] private AudioSource _audioSource;
 
-    private bool _isSignal = false;
+    private IEnumerator _signal;
     private float _minVolume = 0.1f;
     private float _maxVolume = 1f;
-    private float _stepLoudVolume = 0.35f;
+    private float _stepLoudVolume = 0.1f;
+    private float _waitingSeconds = 0.3f;
 
     private void Update()
     {
-        if (_isSignal)
-        {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _maxVolume, _stepLoudVolume * Time.deltaTime);
-        }
-        else if (_audioSource.isPlaying && _audioSource.volume > _minVolume)
-        {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _minVolume, _stepLoudVolume * Time.deltaTime);
-        }
-        else if (_audioSource.isPlaying && _audioSource.volume <= _minVolume)
+        if (_signal != null && _audioSource.isPlaying && _audioSource.volume <= _minVolume)
         {
             _audioSource.Stop();
+            StopCoroutine(_signal);
         }
     }
 
@@ -31,8 +26,9 @@ public class SignalEvent : MonoBehaviour
     {
         if (collision.TryGetComponent<Thief>(out Thief thief))
         {
-            _isSignal = true;
             _reached?.Invoke();
+            _signal = ChangeSignalVolume(_maxVolume);
+            StartCoroutine(_signal);
         }
     }
 
@@ -40,7 +36,24 @@ public class SignalEvent : MonoBehaviour
     {
         if (collision.TryGetComponent<Thief>(out Thief thief))
         {
-            _isSignal = false;
+            if (_signal != null)
+            {
+                StopCoroutine(_signal);
+            }
+
+            _signal = ChangeSignalVolume(_minVolume);
+            StartCoroutine(_signal);
+        }
+    }
+
+    private IEnumerator ChangeSignalVolume(float target)
+    {
+        var wait = new WaitForSeconds(_waitingSeconds);
+
+        while (_audioSource.volume != target)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, target, _stepLoudVolume);
+            yield return wait;
         }
     }
 }
--- JuniorUnity/L5/Assets/Scripts/WalkThereAndBack.cs
+++ JuniorUnity/L5/Assets/Scripts/WalkThereAndBack.cs
@@ -7,18 +7,19 @@ public class WalkThereAndBack : MonoBehaviour
     [SerializeField] private Transform _target;
     private bool _isGoBack = false;
     private float _speed = 1f;
+    private int _digits = 1;
 
     void Update()
     {
-        if (Math.Round(transform.position.y, 1) != Math.Round(_target.position.y, 1) && _isGoBack == false)
+        if (Math.Round(transform.position.y, _digits) != Math.Round(_target.position.y, _digits) && _isGoBack == false)
         {
             transform.position = Vector3.Lerp(transform.position, _target.position, _speed * Time.deltaTime);
         }
-        else if (Math.Round(transform.position.y, 1) == Math.Round(_target.position.y, 1) && _isGoBack == false)
+        else if (Math.Round(transform.position.y, _digits) == Math.Round(_target.position.y, _digits) && _isGoBack == false)
         {
             _isGoBack = true;
         }
-        else if (Math.Round(transform.position.y, 1) != Math.Round(_startPosition.position.y, 1) && _isGoBack)
+        else if (Math.Round(transform.position.y, _digits) != Math.Round(_startPosition.position.y, _digits) && _isGoBack)
         {
             transform.position = Vector3.Lerp(transform.position, _startPosition.position, _speed * Time.deltaTime);
         }
